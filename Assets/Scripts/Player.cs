870ac98b75d479ad15e35cf096dd1ee3589e94ad
noobMisaka <1352270353@qq.com>
2024-02-19 14:11:58 +0800

    添加角色左右移动、所有物体的碰撞体积

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    float moveSpeed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左右移动
+        Transform transform = GetComponent<Transform>();
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(moveSpeed*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-moveSpeed*Time.deltaTime, 0, 0);
+        }
+
+        
+    }
+
+    //碰撞判断
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("碰到了" + other.gameObject.tag);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "DeathLine")
+        {
+            Debug.Log("寄");
+        }
+    }
+}
